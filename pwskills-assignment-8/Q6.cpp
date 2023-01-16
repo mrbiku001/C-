de0544dc6fb2d350a02fe8b3622201b6bd237230
Bikash <contact.biku001@gmail.com>
2023-01-16 20:52:47 +0530

    modified assignment-8

--- pwskills-assignment-8/Q6.cpp
+++ pwskills-assignment-8/Q6.cpp
@@ -1,7 +1,7 @@
 // Q6 - Given a vector array nums, print the count of triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == x. Where k is an integer given by the user.(Hard)
 //     Note: The solution set must not contain duplicate triplets and should not have 3 loops.
 //     Input:[-1,0,1,2,-1,-4] x=0
-//     Output:2
+//     Output:3
 //     Explanation:The two triplets are: -1,0,1 and 1,2,-1
 //     Input:[1,2,3] x=5
 //     Output:0
